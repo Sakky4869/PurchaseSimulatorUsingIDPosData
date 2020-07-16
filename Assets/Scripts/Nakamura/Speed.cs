e77112eee0812c70f94dff853ec740ca3e286819
nanonon <67460996+nanonon@users.noreply.github.com>
2020-07-16 16:10:07 +0900

    button

---
+++ Assets/Scripts/Nakamura/Speed.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Speed : MonoBehaviour
+{
+    [SerializeField]
+    private Slider speed_meter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed_meter.onValueChanged.AddListener(Speed_meter);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Speed_meter(float data)
+    {
+        Debug.Log(data);
+    }
+}

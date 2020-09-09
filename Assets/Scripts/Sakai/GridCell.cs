234d5d0b9ee93bf887a9f445950811135075d543
Sakky4869 <jcjmwh80.private@gmail.com>
2020-09-09 17:15:00 +0900

    ヒートマップ機能追加＋編集用のシーン作成＋障害物の３Ｄオブジェクトの作成

--- Assets/Scripts/Sakai/GridCell.cs
+++ Assets/Scripts/Sakai/GridCell.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GridCell : MonoBehaviour
 {
@@ -19,28 +20,65 @@ public class GridCell : MonoBehaviour
     /// <summary>
     /// ヒートマップの生成スクリプト
     /// </summary>
+    [SerializeField]
     private HeatMap heatMap;
 
+    /// <summary>
+    /// 
+    /// </summary>
     [SerializeField]
     private Color[] myColor;
 
+    /// <summary>
+    /// このグリッドセルを通った回数
+    /// </summary>
+    [SerializeField]
     private int traceCount;
 
-    [SerializeField]
-    private MeshRenderer mesh;
+    //[SerializeField]
+    //private MeshRenderer mesh;
+
+    /// <summary>
+    /// このグリッドセルのイメージの色
+    /// </summary>
+    //[SerializeField]
+    public Image heatMapCellImage;
 
     
     
 
     void Start()
     {
-        heatMap = GameObject.Find("HeatMapCreator").GetComponent<HeatMap>();
-        //myColor = new Color[] { Color.red, Color.blue, Color.black };
+        
     }
 
     void Update()
     {
         
+    }
+
+    public void Init()
+    {
+        heatMap = GameObject.Find("HeatMapCreator").GetComponent<HeatMap>();
+    }
+
+    /// <summary>
+    /// グリッドのデータの最大値と最小値とこのセルの値からヒートマップでの色を計算
+    /// </summary>
+    /// <param name="minValue">グリッドのデータの最小値</param>
+    /// <param name="maxValue">グリッドのデータの最大値</param>
+    public void SetGridCellImageColor(int minValue, int maxValue)
+    {
+        // グリッドのデータの最大値と最小値とこのセルの値からヒートマップでの色を計算
+        float minV = minValue;
+        float maxV = maxValue;
+        float ratio = 2 * (traceCount - minV) / (maxV - minV);
+        int b = (int)Mathf.Max(0, 255 * (1 - ratio));
+        int r = (int)Mathf.Max(0, 255 * (ratio - 1));
+        int g = 255 - b - r;
+
+        // Imageにセット
+        heatMapCellImage.color = new Color(r, g, b);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -49,11 +87,15 @@ public class GridCell : MonoBehaviour
         if (other.tag != "Agent")
             return;
         traceCount++;
-        if (mesh == null)
-            Debug.Log("mesh renderer is null");
+        heatMap.UpdateHeatMapMatrix(x, y);
+        //SetGridCellImageColor(heatMap.minValueOfAgentTraceGrid, heatMap.maxValueOfAgentTraceGrid);
+        
+        // コメント化理由：Mesh Rendererを使わない方法でやることにしたため
+        //if (mesh == null)
+        //    Debug.Log("mesh renderer is null");
 
-        if (mesh.material == null)
-            Debug.Log("material is null");
-        mesh.material.color = myColor[traceCount % 3];
+        //if (mesh.material == null)
+        //    Debug.Log("material is null");
+        //mesh.material.color = myColor[traceCount % 3];
     }
 }
--- Assets/Scripts/Sakai/HeatMap.cs
+++ Assets/Scripts/Sakai/HeatMap.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class HeatMap : MonoBehaviour
 {
@@ -27,19 +28,42 @@ public class HeatMap : MonoBehaviour
     /// エージェントの行動を記録するグリッドデータの配列
     /// </summary>
     public int[, ] agentTraceGrid;
+
+    [HideInInspector]
+    public int maxValueOfAgentTraceGrid;
+
+    [HideInInspector]
+    public int minValueOfAgentTraceGrid;
     
+    /// <summary>
+    /// ヒートマップグリッドの始点
+    /// </summary>
     [SerializeField]
     private Transform gridOriginTransform;
     
+    /// <summary>
+    /// グリッドセルのオブジェクト
+    /// </summary>
     [SerializeField]
     private GridCell gridCell;
 
+    private List<GridCell> gridCells;
+
+    /// <summary>
+    /// グリッドセルの大きさ
+    /// </summary>
     private int gridCellSize;
+
+    private int num;
 
     void Start()
     {
+        minValueOfAgentTraceGrid = 0;
+        maxValueOfAgentTraceGrid = 0;
+        num = 0;
         gridCellSize = (int)gridCell.transform.localScale.x;
-        agentTraceGrid = new int[1280 / gridCellSize, 850 / gridCellSize];
+        agentTraceGrid = new int[850 / gridCellSize, 1280 / gridCellSize];
+        gridCells = new List<GridCell>();
         InstantiateAgentTraceGrid(agentTraceGrid);
     }
 
@@ -47,7 +71,10 @@ public class HeatMap : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ShowHeatMap();
+        }
     }
     
     /// <summary>
@@ -56,17 +83,19 @@ public class HeatMap : MonoBehaviour
     /// <param name="matrix">グリッドデータの配列</param>
     private void InstantiateAgentTraceGrid(int[, ] matrix)
     {
-		for(int y = 0; y < matrix.GetLength(1); y++)
+		for(int y = 0; y < matrix.GetLength(0); y++)
 		{
-			for(int x = 0; x < matrix.GetLength(0); x++)
+			for(int x = 0; x < matrix.GetLength(1); x++)
 			{
 				Vector3 pos = gridOriginTransform.position;
 				pos.x += x * gridCellSize;
 				pos.z -= y * gridCellSize;
 				GameObject cellObject = Instantiate(gridCell.gameObject, pos, Quaternion.identity);
                 GridCell cell = cellObject.GetComponent<GridCell>();
+                gridCells.Add(cell);
 				cell.x = x;
 				cell.y = y;
+                cell.Init();
 			}
 		}
 	}
@@ -78,6 +107,37 @@ public class HeatMap : MonoBehaviour
     /// <param name="agentPosY">エージェントのY座標</param>
     public void UpdateHeatMapMatrix(int agentPosX, int agentPosY)
     {
+        // たどったところのグリッドの値を更新
 		agentTraceGrid[agentPosY, agentPosX] ++;
+
+        // もし現在のグリッドの最大値よりも大きかったら最小値を更新
+        if (maxValueOfAgentTraceGrid < agentTraceGrid[agentPosY, agentPosX])
+        {
+            maxValueOfAgentTraceGrid = agentTraceGrid[agentPosY, agentPosX];
+        }
+
+        // もし現在のグリッドの最小値よりも小さかったら最小値を更新
+        if(minValueOfAgentTraceGrid > agentTraceGrid[agentPosY, agentPosX])
+        {
+            minValueOfAgentTraceGrid = agentTraceGrid[agentPosY, agentPosX];
+        }
+    }
+
+    public void ShowHeatMap()
+    {
+        foreach(GridCell cell in gridCells)
+        {
+            cell.SetGridCellImageColor(minValueOfAgentTraceGrid, maxValueOfAgentTraceGrid);
+            if(num == 0)
+            {
+                cell.heatMapCellImage.gameObject.SetActive(true);
+            }
+            else
+            {
+                cell.heatMapCellImage.gameObject.SetActive(false);
+            }
+        }
+
+        num = 1 - num;
     }
 }
